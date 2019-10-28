f2a8b1917807503d12963d35bb10ec84e3da1522
FastShark <fastshark2008@yandex.ru>
2019-10-28 16:58:30 +0400

    3

--- flut_4/lib/main.dart
+++ flut_4/lib/main.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/material.dart';
 
+class MyBody extends StatelessWidget {
+  Widget build (BuildContext context) {
+    return Center (
+      child: Column (
+        children: <Widget>[
+          Text('What the Fuck?'),
+          FlatButton(onPressed: null,
+            child: Text('PUSH ME'),
+            color: Colors.indigo,
+            textColor: Colors.white,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar(title: MyTitle()),
-      body: Container(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: MyApp(),
+        appBar: AppBar(
+          backgroundColor: Colors.indigo,
+          title: Text('TESTING VERSION'),
+          centerTitle: true,
+        ),
+      ),
     );
   }
-  }
-
-class MyTitle extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return Center(
-        child: Text('SIMPLE TEXT'),
-    );
- }
 }
+
+// class PhContact extends StatelessWidget {
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Image(
+//      image: Image.asset('image/photoContact.jpg'),
+//      fit: BoxFit.cover,
+//    );
+//  }
+// }
